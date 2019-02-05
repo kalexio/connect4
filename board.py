b5e7b2a76c89a9c67369aecc825f0ec3a6a10935
antonia <lotsari@ceid.upatras.gr>
2019-02-05 20:55:19 +0200

    Converted 2d array of ints into 2d array of objects of class Piece

--- board.py
+++ board.py
@@ -4,7 +4,7 @@ import random
 class Board:
     def __init__(self):
         self.players = []
-        self.pieces = [[0 for x in range(7)] for y in range(6)]
+        self.pieces = [[Piece() for y in range(7)] for x in range(6)]
 
     def add_player(self):
         name = input("Enter player's name: ")
@@ -26,7 +26,10 @@ class Board:
 
     def print_board(self):
         for x in range(6):
-            print(self.pieces[x])
+            print(end = "|")
+            for y in range(7):
+                print(self.pieces[x][y].color, end = "|")
+            print()
 
     def next_turn(self, turn):
         choice = False
@@ -35,21 +38,21 @@ class Board:
             choice = self.check_move(column, turn)
 
         row = self.add_piece(column, turn)
-        if self.check_winner(row, column, turn + 1):
-            print("\nCongratulations {0}! You won the game".format(self.players[turn].name))
+        if self.check_winner(row, column, turn):
+            print("\nCongratulations {0}! You won the game!".format(self.players[turn].name))
             return True
 
     def add_piece(self, column, turn):
         for row in range(5,-1,-1):
-            if self.pieces[row][column - 1] != 0:
+            if self.pieces[row][column - 1].color != "0":
                 continue
             else:
-                self.pieces[row][column - 1] = turn + 1
+                self.pieces[row][column - 1].color = self.players[turn].color
                 self.print_board()
                 return row
 
     def check_move(self, column, turn):
-        if self.pieces[0][column - 1] != 0:
+        if self.pieces[0][column - 1].color != "0":
             print("\nNot a valid move! Please select again")
             return False
 
@@ -67,14 +70,14 @@ class Board:
 
     def horizontal_check(self, row, turn):
         for col in range(4):
-            if self.pieces[row][col] == turn and self.pieces[row][col+1] == turn and self.pieces[row][col+2] == turn and self.pieces[row][col+3] == turn:
+            if self.pieces[row][col].color == self.players[turn].color and self.pieces[row][col+1].color == self.players[turn].color and self.pieces[row][col+2].color == self.players[turn].color and self.pieces[row][col+3].color == self.players[turn].color:
                 return True
 
         return False
 
     def vertical_check(self, col, turn):
         for row in range(5,2,-1):
-            if self.pieces[row][col-1] == turn and self.pieces[row-1][col-1] == turn and self.pieces[row-2][col-1] == turn and self.pieces[row-3][col-1] == turn:
+            if self.pieces[row][col-1].color == self.players[turn].color and self.pieces[row-1][col-1].color == self.players[turn].color and self.pieces[row-2][col-1].color == self.players[turn].color and self.pieces[row-3][col-1].color == self.players[turn].color:
                 return True
 
         return False
@@ -82,19 +85,19 @@ class Board:
     def diagonal_check(self, turn):
         for c in range(4):
             for r in range(5,2,-1):
-                if self.pieces[r][c] == turn and self.pieces[r-1][c+1] == turn and self.pieces[r-2][c+2] == turn and self.pieces[r-3][c+3] == turn:
+                if self.pieces[r][c].color == self.players[turn].color and self.pieces[r-1][c+1].color == self.players[turn].color and self.pieces[r-2][c+2].color == self.players[turn].color and self.pieces[r-3][c+3].color == self.players[turn].color:
                     return True
 
         for c in range(4):
             for r in range(0,3):
-                if self.pieces[r][c] == turn and self.pieces[r+1][c+1] == turn and self.pieces[r+2][c+2] == turn and self.pieces[r+3][c+3] == turn:
+                if self.pieces[r][c].color == self.players[turn].color and self.pieces[r+1][c+1].color == self.players[turn].color and self.pieces[r+2][c+2].color == self.players[turn].color and self.pieces[r+3][c+3].color == self.players[turn].color:
                     return True
 
         return False
 
 class Piece:
     def __init__(self):
-        self.color = None
+        self.color = "0"
 
 class Player:
     def __init__(self, name):
